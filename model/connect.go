5d5648733353b7ead40b8d17ebae9eef8c746d20
darthVader18 <50162503+darthVader18@users.noreply.github.com>
2020-05-25 17:49:01 +0530

    heroku

--- model/connect.go
+++ model/connect.go
@@ -9,7 +9,7 @@ import (
 var con *sql.DB
 
 func Connect() *sql.DB {
-	db, err := sql.Open("mysql", "postgres://jyyjzaznhrqazf:7dffe2c89bcc2dd572cf4f246fcf11fad6d1c4ee71fad15bede2261df033aa31@ec2-54-175-117-212.compute-1.amazonaws.com:5432/d8kvaiv71qft39")
+	db, err := sql.Open("mysql", "ec2-54-175-117-212.compute-1.amazonaws.com")
 	if err != nil {
 		log.Fatal(err)
 	}
